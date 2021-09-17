34db62ef798ab42500ea30eca5ec9e69bf5b80aa
Shubhangi Gondage <78039310+ShubhangiGondage@users.noreply.github.com>
2021-09-17 09:14:12 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -3,22 +3,25 @@
 <?php 
 // $name="";
 if (isset($_POST['submit'])) {
+    $mailto = "tsfdonation@gmail.com";
     $name = $_POST['name'];
     $visitor_email = $_POST['email'];
     $message = $_POST['message'];
+   
 
     $email_subject = 'New form submission';
 
     $email_body = "User Name: $name.\n" . "User Email: $visitor_email.\n" .  "User message:$message .\n";
 
-    $headers = "From: gfgforshubh@gmail.com";
+    $headers = "From: $visitor_email";
+    $headers2 = "From: $mailto";
 
     
-    $to = 'shubhangigondage680@gmail.com';
+//$to = 'shubhangigondage680@gmail.com';
 
 
-    mail($to, $email_subject, $email_body,$headers );
-    mail($visitor_email,$email_subject, $email_body);
+    mail($mailto, $email_subject, $email_body,$headers );
+    mail($visitor_email,$email_subject, $email_body,$headers2 );
     header("Location: index.php");
     
 }
